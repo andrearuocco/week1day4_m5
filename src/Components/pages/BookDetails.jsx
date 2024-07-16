ffe09620851e7e0684dacf4a3f25e145a3c8d5d6
andrearuocco <ruoca96@gmail.com>
2024-07-16 18:01:29 +0200

    bookDetfungerà

--- src/Components/pages/BookDetails.jsx
+++ src/Components/pages/BookDetails.jsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
 import CommentArea from "../CommentArea";
+import books from '../data/history.json'
+
 
 function BookDetails() {
     const { asin } = useParams()
     return (
         <>
-            <h1> sono il dettaglio n {asin} </h1>
 
+            <h1> sono il dettaglio n {asin} </h1>
+            {books.map(book => book.asin === {asin}.asin ? <h1>ciao</h1> : "")}
             <CommentArea asin={asin} />
         </>
 
