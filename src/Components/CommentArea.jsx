c771a95db5e87a1b373d3ad4ace9357323472b92
andrearuocco <ruoca96@gmail.com>
2024-07-20 19:02:33 +0200

    spinner

--- src/Components/CommentArea.jsx
+++ src/Components/CommentArea.jsx
@@ -5,19 +5,28 @@ import './CommentArea.css'
 
 function CommentArea({asin}) {
     const [comments, setComments] = useState([])
+    const [loading, setLoading] = useState(false)
     const loadComments = async () => {
-        const response = await fetch(`https://striveschool-api.herokuapp.com/api/books/${asin}/comments/`,
-            {
-                headers: {
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjliYzFmYzMxZGI2MDAwMTUwYjliZDQiLCJpYXQiOjE3MjE0ODM3NzIsImV4cCI6MTcyMjY5MzM3Mn0.xFNOKL25iw3c__Ic-n21T4Vg2BJDpM8AiI1Out_mSzg"
-                }
-            })
-        const data = await response.json()
-        setComments(data)
+        setLoading(true)
+        
+        try{
+            const response = await fetch(`https://striveschool-api.herokuapp.com/api/books/${asin}/comments/`,
+                {
+                    headers: {
+                        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjliYzFmYzMxZGI2MDAwMTUwYjliZDQiLCJpYXQiOjE3MjE0ODM3NzIsImV4cCI6MTcyMjY5MzM3Mn0.xFNOKL25iw3c__Ic-n21T4Vg2BJDpM8AiI1Out_mSzg"
+                    }
+                })
+            const data = await response.json()
+            setComments(data)
+        }
+        finally{
+            setLoading(false)
+        }
+        
     }
     useEffect(() => {loadComments()}, [asin])
     return (
-        <div className="sticky-top p-3 bor-com-ar"> <AddComment asin={asin} loadComments={loadComments}/> <CommentList comments={comments} loadComments={loadComments}/> </div>
+        <div className="sticky-top p-3 bor-com-ar"> <AddComment asin={asin} loadComments={loadComments}/> <CommentList comments={comments} loadComments={loadComments} loading={loading}/> </div>
     )
 }
 
--- src/Components/CommentList.jsx
+++ src/Components/CommentList.jsx
@@ -1,7 +1,44 @@
 import SingleComment from "./SingleComment"
 import "./CommentList.css"
+import { Spinner, Button } from "react-bootstrap";
 
-function CommentList({comments, loadComments}) {
+
+function CommentList({comments, loadComments, loading}) {
+    if (loading){
+        return (<>
+            <Button variant="danger" disabled>
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              <span className="visually-hidden">Loading...</span>
+            </Button>{' '}
+            <Button variant="danger" disabled>
+              <Spinner
+                as="span"
+                animation="grow"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Loading...
+            </Button>
+          </>)
+    }
+    /* const [isLoading, setIsLoading] = useState(false);
+    if (comments.length === 0) {
+        setIsLoading(true)
+    } */
+    /*   if (isLoading)
+      return(
+        <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+    </Spinner>
+      )
+ */
     return(
         <div className="commentList"><h3 className='text-center'>I vostri commenti</h3>{comments.map(comment => <SingleComment key={comment._id} comment={comment} loadComments={loadComments}/>)}</div>  
     )
